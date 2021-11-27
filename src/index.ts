26b61fba08a8913e8f83900a678cc738519cadfa
Anna Koehler <anna@foundrymakes.com>
2021-11-26 20:43:09 -0600

    express

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,32 @@
 import { MikroORM} from "@mikro-orm/core";
 import {Post} from "./entities/Post";
 import microConfig from "../mikro-orm.config"
+import express from 'express'
 
 const main = async () => {
-    //added await because this returns a promise
     //connect to db using config file
     const orm = await MikroORM.init(microConfig)
-
     //run migrations
     await orm.getMigrator().up()
 
     //create a Post object
     //const post = orm.em.create(Post, {title: 'my first post'})
-
     //update db
     //await orm.em.persistAndFlush(post)
-
     //get posts
     const posts = await orm.em.find(Post,{})
     console.log(posts)
+
+
+    const app = express()
+    //create express get endpoint; _ = param when not used
+    app.get('/', (_, res) => {
+        res.send('hello')
+    })
+    app.listen(4000,() => {
+        console.log('server started on port 4000')
+    })
+
 }
 console.log('hello world')
 
