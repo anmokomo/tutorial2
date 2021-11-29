ae6b77d52359d69dc97a75ac0fafc09b4e9ac041
Anna Koehler <anna@foundrymakes.com>
2021-11-28 22:02:03 -0600

    setup user session (cookie)

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import { MikroORM} from "@mikro-orm/core";
-import {Post} from "./entities/Post";
-import microConfig from "../mikro-orm.config"
-import express from 'express'
+import {ApolloServerPluginLandingPageGraphQLPlayground} from "apollo-server-core";
 import {ApolloServer} from "apollo-server-express";
+import connectRedis from 'connect-redis';
+import express from 'express'
+import session from 'express-session';
+import redis from 'redis';
 import {buildSchema} from 'type-graphql'
+import microConfig from "../mikro-orm.config"
 import {HelloResolver} from "./resolvers/hello.resolver";
 import {PostResolver} from "./resolvers/post.resolver";
 import {UserResolver} from "./resolvers/user.resolver";
+//import {_prod_} from "./constants";
+import {MyContext} from "./types";
+
+declare module "express-session" {
+    interface Session {
+        userId: number
+    }
+}
 
 const main = async () => {
     //connect to db using config file
@@ -19,9 +30,8 @@ const main = async () => {
     //update db
     //await orm.em.persistAndFlush(post)
     //get posts
-    const posts = await orm.em.find(Post,{})
-    console.log(posts)
-
+    // const posts = await orm.em.find(Post,{})
+    // console.log(posts)
 
     const app = express()
     //create express get endpoint; _ = param when not used
@@ -29,24 +39,54 @@ const main = async () => {
     //     res.send('hello')
     // })
 
+    //connect to redis
+    const RedisStore = connectRedis(session)
+    let redisClient
+    try {
+        redisClient = await redis.createClient()
+    } catch(err) {console.log('createClient',err)}
+    //Redis needs to be set up before Apollo, because Redis will be used in the middleware
+    app.use(
+        session({
+            cookie: {
+                httpOnly: true, //in JS code, can't access cookie
+                maxAge: 1000 * 60 * 60 * 24 * 365 * 5,
+                sameSite: 'lax', // csrf
+                //TODO change to false if having problems in prod
+                //secure: _prod_ //cookie only works in https
+            },
+            name: 'qid',
+            resave: false,
+            saveUninitialized: false, //set whether or not to create empty sessions
+            //TODO use env var
+            secret: 'fdskfjdsklfjkl',
+            store: new RedisStore({
+                client: redisClient,
+                disableTouch: true
+            })
+        })
+    )
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [HelloResolver, PostResolver, UserResolver],
             validate: false
         }),
-        //accessible to resolvers
-        context: () => ({ em: orm.em })
+        //instead of web console, can use local console @ 4000
+        plugins: [ApolloServerPluginLandingPageGraphQLPlayground(),],
+        //context func; accessible to resolvers; access to req and res from express sessions
+        context: ({ req, res }): MyContext => ({ em: orm.em, req, res })
     })
 
     //go to <server>/graphql to see graphql console
     await apolloServer.start()
     //create a graphql endpoint on express
-    apolloServer.applyMiddleware({ app })
+    apolloServer.applyMiddleware({ app, cors: {
+            credentials: true,
+        } })
 
     app.listen(4000,() => {
         console.log('server started on port 4000')
     })
-
 }
 console.log('hello world')
 
