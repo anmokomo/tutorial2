1124fac6c87d0e5ebecc7eb502ae9f00fe4f1601
Anna Koehler <anna@foundrymakes.com>
2021-11-28 21:52:45 -0600

    add Req and Res objects to MyContext

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,10 @@
 import {Connection, EntityManager, IDatabaseDriver} from "@mikro-orm/core";
+import {Request, Response } from "express";
+import {Session, SessionData} from "express-session";
 
 //got type from looking at the type of 'orm.em'
 export type MyContext = {
-    em: EntityManager<IDatabaseDriver<Connection>>
+    em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>
+    req: Request & { session: Session & Partial<SessionData>}
+    res: Response
 }
